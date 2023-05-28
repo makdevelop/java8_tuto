ed10fb6df43e1ca67433c43a31ecf93cdaa53824
Wassim <wassimhamrouni123@gmail.com>
2023-05-29 00:55:39 +0200

    Branch wassim (#1)
    * create class Book in new branch
    
    * add class bib
    
    * new mùethod deleteBook
    
    * add get List method
    
    * new mùethod editBook
    
    * delete bib
    
    * new getBookById method
    
    * reset last commit
    
    * Revert "reset last commit"
    
    This reverts commit c63fb5d60c5580173ec14f477e296a0221c04b73.
    
    ---------
    
    Co-authored-by: Intissar <i.bennayaepelfad@soprasteria.com>

---
+++ java8_tuto/src/java8_tuto/Book.java
@@ -0,0 +1,25 @@
+package java8_tuto;
+
+public class Book {
+
+    private int id;
+    private String title;
+
+    public Book(){
+    }
+
+    public void addBook(){
+        System.out.println("book added");
+    }
+    public void getListBook(){
+        System.out.println("book bib");
+}
+    public void deleteBook(){
+        System.out.println("Book deleted !!!");
+    }
+
+    public void editBook(){
+        System.out.println("Book edited !!!");
+    }
+    
+}
---
+++ java8_tuto/src/java8_tuto/bib.java
@@ -0,0 +1,36 @@
+package java8_tuto;
+
+public class Bib {
+
+    private int id;
+    private String title;
+
+    public bib(){
+    }
+
+    public void addBib(){
+        System.out.println("book bib");
+    }
+    
+    public void getListBook(){
+        System.out.println("book bib");
+    }
+    
+
+
+    public void getBookById(){
+        System.out.println("get book by id !!!");
+    }
+
+    public void deleteBiB(){
+        System.out.println("DELETE bib");
+    }
+
+     public void test(){
+        System.out.println("test bib");
+    }
+    public void getAllBib(){
+        System.out.println("get all bib");
+    }
+
+}

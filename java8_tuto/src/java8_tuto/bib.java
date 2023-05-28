f96106aaa81c999f6262fbcad5f50cc4a625ab0d
wassim hamrouni <wassimhamrouni123@gmail.com>
2023-05-28 23:50:34 +0200

    new getBookById method

--- java8_tuto/src/java8_tuto/bib.java
+++ java8_tuto/src/java8_tuto/bib.java
@@ -15,4 +15,9 @@ public class Bib {
     public void getListBook(){
         System.out.println("book bib");
     }
+
+
+    public void getBookById(){
+        System.out.println("get book by id !!!");
+    }
 }
